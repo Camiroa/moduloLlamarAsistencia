475f3343a436e522db716596d5a6fffb41695023
Camiroa <Camila.roa@fiuni.edu.py>
2022-11-03 03:21:54 -0300

    Completo

---
+++ src/main/java/com/fiuni/moduloLlamarAsistencia/controller/Detalles_PAResourse.java
@@ -0,0 +1,48 @@
+package com.fiuni.moduloLlamarAsistencia.controller;
+
+import com.fiuni.moduloLlamarAsistencia.dto.portfolio.planilla.Detalles_PADTO;
+import com.fiuni.moduloLlamarAsistencia.dto.portfolio.planilla.Detalles_PAResult;
+import com.fiuni.moduloLlamarAsistencia.service.Detalles_PA.IDetalles_PAService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping ("/asistencia/detalle")
+public class Detalles_PAResourse {
+    @Autowired
+    private IDetalles_PAService detalles_paService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Detalles_PADTO> getById(@PathVariable(value="id") Integer detallea){
+        return detalles_paService.getById(detallea);
+    }
+
+    @GetMapping(path = "/page/{page_num}")
+    public ResponseEntity<Detalles_PAResult> getPages(@PathVariable(value = "page_num")Integer pageNum) {
+        return detalles_paService.getAll(PageRequest.of(pageNum, 10));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Detalles_PADTO> save(@Validated @RequestBody Detalles_PADTO deta) {
+        return detalles_paService.save(deta);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Detalles_PADTO> putDetalle(@PathVariable(value = "id") Integer id, @RequestBody Detalles_PADTO dto) {
+        return detalles_paService.update(id, dto);
+    }
+
+    @DeleteMapping("eliminar/{id}")
+    public ResponseEntity<Boolean> deleteDetalle(@PathVariable(value = "id") Integer id) {
+        return detalles_paService.delete(id);
+    }
+
+    //@DeleteMapping("/fullDelete/{id}")
+    //public ResponseEntity<Integer> fullDeleteDetalle(@PathVariable(value = "id") Integer id){
+    //   return detalles_paService.fullDelete(id);
+//    }
+}
